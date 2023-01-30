83d971df07b9f462fa8c9ba037504c3d2257d125
aalexander0422 <122483416+aalexander0422@users.noreply.github.com>
2023-01-29 20:46:28 -0500

    h

--- user_numeric_lists.py
+++ user_numeric_lists.py
@@ -6,7 +6,7 @@ Modify this docstring.
 # import some modules first - how many can you make use of?
 
 import math
-
+import statistics
 
 # define some functions
 
@@ -44,6 +44,43 @@ if __name__ == "__main__":
     # call your functions here (see instructions)
     print("your code here")
 
+    List1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    listx = [1,2,3,4,5,6,7,8,9,10]
+    listy = [2,4,6,10,12,14,16,18,20,22]
+    mean1 = statistics.mean(List1)
+    meanx = statistics.mean(listx)
+    meany = statistics.mean(listy)
+    print(f'The mean for list 1 is', {mean1} , ' \n The mean for list x is ', {meanx}, ' \n The mean for list y ', {meany})
+    mode1 = statistics.mode(List1)
+    modex = statistics.mode(listx)
+    modey = statistics.mode(listy)
+    print(f'The mode for list 1 is', {mode1} , ' \n The mode for list x is ', {modex}, ' \n The mode for list y ', {modey})
+    median1 = statistics.median(List1)
+    medianx = statistics.median(listx)
+    mediany = statistics.median(listy)
+    print(f'The median for list 1 is', {median1} , ' \n The median for list x is ', {medianx}, ' \n The median for list y ', {mediany})
+    Stand1 = statistics.stdev(List1)
+    standx = statistics.stdev(listx)
+    standy = statistics.stdev(listy)
+    print(f'The Standard deviation for list 1 is', {Stand1} , ' \n The Standard Deviation for list x is ', {standx}, ' \n The Standard Deviation for list y ', {standy})
+    Var1 = statistics.variance(List1)
+    Varx = statistics.variance(listx)
+    Vary = statistics.variance(listy)
+    print(f'The Variance for list 1 is', {Var1} , ' \n The Variance for list x is ', {Varx}, ' \n The Variance for list y ', {Vary})
+    xy_corr = statistics.correlation(listx, listy)
+    print(f'The correlation between list X and list Y is ', {xy_corr})
+    slope, intercept = statistics.linear_regression(listx, listy)
+    future_x = 15
+    future_y = round(slope * future_x + intercept)
+    print(f'The Y value at the future time 15 is', {future_y})
+    min1 = min(List1)
+    Max1 = max(List1)
+    Len1 = len(List1)
+    Sum1 = sum(List1)
+    set1 = set(List1)
+    average1 = Sum1 / Len1
+    sorted1 = sorted(List1)
+    sorted11 = sorted(List1, reverse=True)
 
 # Why? Why only print if this the module called?
 # Because when you write good functions, you may want to
